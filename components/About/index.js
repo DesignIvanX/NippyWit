5b26579794806728a9f4cef44171476017ac4c8e
ivandcdesign <ivandcdesign@gmail.com>
2022-07-22 19:53:53 -0400

    v1.1

--- components/About/index.js
+++ components/About/index.js
@@ -1,9 +1,16 @@
 import { Button } from "../Button";
+import { motion } from "framer-motion";
 import styles from "./styles.module.css";
 export const About = () => {
   return (
     <section className={styles.about}>
-      <div className={styles.videoContainer}>
+      <motion.div
+        initial={{ x: -100, opacity: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        whileInView={{ x: 0, opacity: 1 }}
+        className={styles.videoContainer}
+      >
         <iframe
           className={styles.video}
           src="https://www.youtube.com/embed/kM_NbktXHLw"
@@ -12,9 +19,15 @@ export const About = () => {
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
-      </div>
+      </motion.div>
 
-      <div className={styles.text}>
+      <motion.div
+        initial={{ x: 100, opacity: 0 }}
+        transition={{ duration: 1 }}
+        viewport={{ once: true }}
+        whileInView={{ x: 0, opacity: 1 }}
+        className={styles.text}
+      >
         <h2 className={styles.h2}>
           Lorem Ipsum is simply dummy text of the printing.
         </h2>
@@ -23,7 +36,7 @@ export const About = () => {
           industry. Lorem Ipsum has been the industry.
         </p>
         <Button type="fill">Portafolio</Button>
-      </div>
+      </motion.div>
     </section>
   );
 };

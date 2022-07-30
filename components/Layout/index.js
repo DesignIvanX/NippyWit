9bb8981caa7b7ff0e8ed3578f07153874b964ac5
ivandcdesign <ivandcdesign@gmail.com>
2022-07-29 21:13:41 -0400

    v1.0

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,7 +1,7 @@
 import { Navegation } from "../Navegation";
 import { Footer } from "../Footer";
 import { Line } from "../Line";
-
+import { WhatsApp } from "../WhatsApp";
 import styles from "./styles.module.css";
 export const Layout = ({ children }) => {
   return (
@@ -9,6 +9,7 @@ export const Layout = ({ children }) => {
       <Line />
       <Navegation />
       {children}
+      <WhatsApp />
       <Footer />
     </main>
   );

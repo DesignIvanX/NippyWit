1fb2416e8ccbcd95e35e3ac16bb2e102bf4150c9
ivandcdesign <ivandcdesign@gmail.com>
2022-07-31 21:09:15 -0400

    v1.1

--- components/WhatsApp/index.js
+++ components/WhatsApp/index.js
@@ -1,21 +1,18 @@
+import { Logo } from "./Logo";
 import { WhatsAppWidget } from "react-whatsapp-widget";
 import "react-whatsapp-widget/dist/index.css";
-import { Logo } from "./Logo";
-import styles from "./styles.module.css";
 
 export const WhatsApp = ({ number }) => {
   return (
-    <div className={styles.whatsapp}>
-      <WhatsAppWidget
-        CompanyIcon={Logo}
-        phoneNumber={number}
-        companyName="Nippy Wit"
-        message="Hello! 👋🏼 Queremos crecer tu negocio!"
-        replyTimeText="Agencia de Marketing Digital"
-        inputPlaceHolder="Escribenos un mensage"
-        sendButtonText="Enviar"
-        open="true"
-      />
-    </div>
+    <WhatsAppWidget
+      CompanyIcon={Logo}
+      phoneNumber={number}
+      companyName="Nippy Wit"
+      message="Hello! 👋🏼 Queremos crecer tu negocio!"
+      replyTimeText="Agencia de Marketing Digital"
+      inputPlaceHolder="Escribenos un mensage"
+      sendButtonText="Enviar"
+      open="true"
+    />
   );
 };

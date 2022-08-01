48a11d81f034cf3dcb46e28fb7f810449d7648b6
ivandcdesign <ivandcdesign@gmail.com>
2022-07-31 21:36:36 -0400

    v1.1

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -2,6 +2,7 @@ import { Navegation } from "../Navegation";
 import { Footer } from "../Footer";
 import { Line } from "../Line";
 import { WhatsApp } from "../WhatsApp";
+import { Loader } from "../Loader";
 import styles from "./styles.module.css";
 export const Layout = ({ children }) => {
   return (
@@ -10,6 +11,7 @@ export const Layout = ({ children }) => {
       <Navegation />
       {children}
       <WhatsApp number="+595961154054" />
+      <Loader />
       <Footer />
     </main>
   );
---
+++ components/Loader/index.js
@@ -0,0 +1,24 @@
+import { motion } from "framer-motion";
+import Image from "next/image";
+import styles from "./styles.module.css";
+export const Loader = () => {
+  return (
+    <motion.div
+      initial={{ x: 0, opacity: 1 }}
+      transition={{ delay: 3, duration: 1 }}
+      animate={{ x: "-100%" }}
+      className={styles.loader}
+    >
+      <motion.img
+        initial={{ x: 0, opacity: 1 }}
+        transition={{ repeat: Infinity, duration: 1 }}
+        animate={{ rotate: [0, 360] }}
+        className={styles.image}
+        src="/static/logo.png"
+        alt="Nippy Wit Logo"
+        width={150}
+        height={100}
+      />
+    </motion.div>
+  );
+};

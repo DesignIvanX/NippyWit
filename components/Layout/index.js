b9ee6be046035cd83cc8e9fe860f8673b2038486
ivandcdesign <ivandcdesign@gmail.com>
2022-08-10 18:37:55 -0400

    v1.1

---
+++ components/Layout/index.js
@@ -0,0 +1,20 @@
+import { Navegation } from "../Navegation";
+import { Footer } from "../Footer";
+import { Line } from "../Line";
+import { WhatsApp } from "../WhatsApp";
+import { Loader } from "../Loader";
+import { Alert } from "../Alert";
+import styles from "./styles.module.css";
+export const Layout = ({ children }) => {
+  return (
+    <main className={styles.layout} id="layout">
+      <Line />
+      <Navegation />
+      {children}
+      <WhatsApp number="+595961154054" />
+      <Loader />
+      <Alert />
+      <Footer />
+    </main>
+  );
+};

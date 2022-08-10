26c0b4858b5bbef36467cb2572025f53eddd7e6a
ivandcdesign <ivandcdesign@gmail.com>
2022-08-10 19:52:59 -0400

    v1.0

--- components/Alert/index.js
+++ components/Alert/index.js
@@ -3,9 +3,16 @@ import Image from "next/image";
 import { Button } from "../Button";
 import styles from "./styles.module.css";
 export const Alert = () => {
+  const handleOnClick = () => {
+    const alert = document.querySelector("#alert");
+    alert.classList.remove("displayBlock");
+  };
   return (
     <section className={styles.alert} id="alert">
-      <motion.div>
+      <motion.div
+        transition={{ repeat: Infinity, duration: 4 }}
+        animate={{ rotate: [-40, 40, 0, 40, -40] }}
+      >
         <Image
           src="/static/alert/hand.svg"
           alt="mano 3d"
@@ -15,7 +22,9 @@ export const Alert = () => {
       </motion.div>
 
       <p className={styles.p}>Subscrito correctamente!</p>
-      <Button>OK!</Button>
+      <Button path="/services/" onClick={handleOnClick}>
+        OK!
+      </Button>
     </section>
   );
 };

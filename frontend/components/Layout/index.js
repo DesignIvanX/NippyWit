3b29749534f4b01ee647789eee8b5b28e92a5eff
ivandcdesign <ivandcdesign@gmail.com>
2022-08-10 14:10:43 -0400

    v1.5

--- frontend/components/Layout/index.js
+++ frontend/components/Layout/index.js
@@ -3,7 +3,7 @@ import { Footer } from "../Footer";
 import { Line } from "../Line";
 import { WhatsApp } from "../WhatsApp";
 import { Loader } from "../Loader";
-import { Alert } from "../Alert";
+// import { Alert } from "../Alert";
 import styles from "./styles.module.css";
 export const Layout = ({ children }) => {
   return (
@@ -13,7 +13,7 @@ export const Layout = ({ children }) => {
       {children}
       <WhatsApp number="+595961154054" />
       <Loader />
-      <Alert />
+      {/* <Alert /> */}
       <Footer />
     </main>
   );
